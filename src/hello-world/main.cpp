f11203fcbdbf53a976b4ddd498925316b85531ba
Fernando Bevilacqua <dovyski@gmail.com>
2019-02-18 10:18:43 -0300

    Add basic usage for OpenGL and GLFW

--- src/hello-world/main.cpp
+++ src/hello-world/main.cpp
@@ -1,5 +1,54 @@
+/**
+ * Basic usege of OpenGL and GLFW. 
+ */
+
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+#define WIDTH 500
+#define HEIGHT 500
 
 int main() {
+	GLFWwindow* window;
+	int width, height;
+
+	if (!glfwInit()) {
+		std::cout << "Problem to initialize GLFW" << std::endl;
+		exit(1);
+	}
+	
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Hello world, GLFW!", NULL, NULL);
+
+	if (!window) {
+		std::cout << "Problem to create GLFW window" << std::endl;
+		glfwTerminate();
+		exit(1);
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while(glfwWindowShouldClose(window) == 0 && glfwGetKey(window, GLFW_KEY_ESCAPE) == 0) {
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+
+		glBegin(GL_TRIANGLES);
+			glColor3f(1.f, 0.f, 0.f);
+			glVertex3f(-0.6f, -0.4f, 0.f);
+			glColor3f(0.f, 1.f, 0.f);
+			glVertex3f(0.6f, -0.4f, 0.f);
+			glColor3f(0.f, 0.f, 1.f);
+			glVertex3f(0.f, 0.6f, 0.f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+    glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
