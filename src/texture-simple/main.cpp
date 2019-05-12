ae61b304d1a940512773a210efb0670506544233
Fernando Bevilacqua <dovyski@gmail.com>
2019-05-11 22:04:26 -0300

    Add new demo about texture

---
+++ src/texture-simple/main.cpp
@@ -0,0 +1,126 @@
+/**
+ * Demonstrate the use of textures.
+ */
+
+#include <GL/glut.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "Bmp.h"
+
+#define SCREEN_WIDTH 900
+#define SCREEN_HEIGHT 300
+#define RECT_SIZE 10
+
+GLuint texture_id;
+Bmp *texture_bmp;
+unsigned char *texture_data;
+
+float ang = 0;
+
+void buildTextures()
+{
+	glGenTextures(1, &texture_id);
+	glBindTexture(GL_TEXTURE_2D, texture_id);
+
+	float scale = 1.0 / 10;
+	float p1[4] = { scale, 0, 0, 0 };
+	float p2[4] = { 0, scale, 0, 0 };
+	glTexGenfv(GL_S, GL_OBJECT_PLANE, p1);
+	glTexGenfv(GL_T, GL_OBJECT_PLANE, p2);
+
+	glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+	glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	gluBuild2DMipmaps(GL_TEXTURE_2D,
+		GL_RGB,
+		texture_bmp->getWidth(),
+		texture_bmp->getHeight(),
+		GL_RGB,
+		GL_UNSIGNED_BYTE,
+		texture_data);
+}
+
+void display(void)
+{
+   float aperture = 35;
+   float znear = 1;
+   float zfar= 200;
+
+   ang += 0.1;
+
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity( );
+   gluPerspective(aperture, SCREEN_WIDTH/SCREEN_HEIGHT, znear, zfar);
+
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   
+   glTranslated(-60, 0, -120);
+   glRotated(ang, 0, 1, 0);
+   glScalef(5, 5, 5);
+   glTranslated(-RECT_SIZE / 2, -RECT_SIZE / 2, 0);
+
+   glBindTexture(GL_TEXTURE_2D, texture_id);
+
+   glEnable(GL_TEXTURE_GEN_S);
+   glEnable(GL_TEXTURE_GEN_T);
+   
+   glBegin(GL_QUADS);
+	   glVertex3f(0, 0, 0);
+	   glVertex3f(RECT_SIZE, 0, 0);
+	   glVertex3f(RECT_SIZE, RECT_SIZE, 0);
+	   glVertex3f(0, RECT_SIZE, 0);
+   glEnd();
+
+   glutSwapBuffers();
+}
+
+void init()
+{
+	glClearColor(0, 0, 0, 1);
+
+	glShadeModel(GL_SMOOTH);
+	glDepthFunc(GL_LESS);
+
+	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
+
+	texture_bmp = new Bmp("data/texture.bmp");
+	texture_bmp->convertBGRtoRGB();
+	texture_data = texture_bmp->getImage();
+
+	if (texture_data != NULL) {
+		glEnable(GL_TEXTURE);
+		glEnable(GL_TEXTURE_2D);
+		buildTextures();
+	}
+}
+
+
+int main (int argc, char *argv[])
+{
+   glutInit(&argc, argv);
+
+   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
+   glutInitWindowSize (SCREEN_WIDTH, SCREEN_HEIGHT);
+   glutCreateWindow ("Texture simple");
+
+   init();
+
+   glutDisplayFunc(display);
+   glutIdleFunc(display);
+
+   glutMainLoop();
+   return 0;
+}
