87eecc9397d4c64b1d5c168c43c0abff0408252c
Fernando Bevilacqua <dovyski@gmail.com>
2019-11-26 15:43:44 -0300

    Improve and simplifies shader demo

--- src/threejs-shader/shader.js
+++ src/threejs-shader/shader.js
@@ -5,120 +5,85 @@
 
 window.addEventListener('load', init);
 
-let scene
-let camera
-let renderer
-let sceneObjects = []
-let uniforms = {}
+let scene;
+let camera;
+let renderer;
+
+let uniforms = {
+    colorA: { type: 'vec3', value: new THREE.Color(0xff0000) },
+    colorB: { type: 'vec3', value: new THREE.Color(0x00ff00) }
+};
+
+let vertexShader = `
+    varying vec4 modelViewPosition; 
+
+    void main() {
+        modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+        gl_Position = projectionMatrix * modelViewPosition; 
+    }
+`;
+
+let fragmentShader = `
+    uniform vec3 colorA; 
+    uniform vec3 colorB; 
+
+    void main() {
+        vec3 finalColor = colorA + colorB;
+        gl_FragColor = vec4(finalColor, 1.0);
+    }
+`;
 
 function init() {
-    scene = new THREE.Scene()
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.z = 5
-
-    renderer = new THREE.WebGLRenderer()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-
-    document.body.appendChild(renderer.domElement)
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
     
-    addBasicCube()
-    addExperimentalCube()
+    addBasicCube();
+    addShadingCube();
 
-    animationLoop()
+    animationLoop();
 }
 
 function addBasicCube() {
     let geometry = new THREE.BoxGeometry(1, 1, 1);
-    let material = new THREE.ShaderMaterial();
+    var material = new THREE.MeshBasicMaterial({color: 0x0000ff});
 
-    let mesh = new THREE.Mesh(geometry, material)
-    mesh.position.x = -2
-    scene.add(mesh)
-    sceneObjects.push(mesh)
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = -2;
+    scene.add(mesh);
 }
 
-function vertexShader() {
-    return `
-    varying vec4 modelViewPosition; 
-
-    void main() {
-      modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-      gl_Position = projectionMatrix * modelViewPosition; 
-    }
-    `;
-}
-
-function fragmentShader() {
-    return `
-      uniform vec3 colorA; 
-      uniform vec3 colorB; 
-
-      void main() {
-        vec3 finalColor = colorA + colorB;
-        gl_FragColor = vec4(finalColor, 1.0);
-      }
-    `;
-}
-
-
-function fragmentShaderOneColorOnly() {
-    return `
-      uniform vec3 colorA; 
-      uniform vec3 colorB; 
-
-      void main() {
-        gl_FragColor = vec4(colorA, 1.0);
-      }
-    `;
-}
-
-function addExperimentalCube() {
-    uniforms.colorA = { type: 'vec3', value: new THREE.Color(0xff0000) }
-    uniforms.colorB = { type: 'vec3', value: new THREE.Color(0x00ff00) }
+function addShadingCube() {
+    uniforms.colorA = { type: 'vec3', value: new THREE.Color(0xff0000) };
+    uniforms.colorB = { type: 'vec3', value: new THREE.Color(0x00ff00) };
 
     let geometry = new THREE.BoxGeometry(1, 1, 1);
 
     let material = new THREE.ShaderMaterial({
         uniforms: uniforms,
-        fragmentShader: fragmentShader(),
-        vertexShader: vertexShader(),
+        fragmentShader: fragmentShader,
+        vertexShader: vertexShader,
     });
 
     let mesh = new THREE.Mesh(geometry, material);
 
     mesh.position.x = 2;
     scene.add(mesh);
-    sceneObjects.push(mesh);
 }
-
-function addExperimentalLightCube() {
-    uniforms.colorA = { type: 'vec3', value: new THREE.Color(0x74ebd5) }
-    uniforms.colorB = { type: 'vec3', value: new THREE.Color(0xACB6E5) }
-
-    let geometry = new THREE.BoxGeometry(1, 1, 1)
-    let material = new THREE.ShaderMaterial({
-        uniforms: uniforms,
-        fragmentShader: fragmentShaderOneColorOnly(),
-        vertexShader: vertexShader(),
-        lights: true
-    })
-
-    let mesh = new THREE.Mesh(geometry, material)
-    mesh.position.x = 2
-    scene.add(mesh)
-    sceneObjects.push(mesh);
-}
-
 
 function animationLoop() {
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 
-    for (let object of sceneObjects) {
-        object.rotation.x += 0.01;
-        object.rotation.y += 0.03;
-    }
+    scene.traverse(function(node) {
+        if(node instanceof THREE.Mesh) {
+            node.rotation.x += 0.01;
+            node.rotation.y += 0.03;
+        }
+    });
 
-    requestAnimationFrame(animationLoop)
+    requestAnimationFrame(animationLoop);
 }
-
